25118d953eea28186a1193ac4f24190b443f4fcd
prugved993-sudo <prugved993@gmail.com>
2026-08-01 21:16:33 +0530

    Update vector_store.py

--- ResearchGPT/video_assistant/core/vector_store.py
+++ ResearchGPT/video_assistant/core/vector_store.py
@@ -1,5 +1,5 @@
 import os
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
